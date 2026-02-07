6be6f6f44330353e4a738ff945498efb53e003bc
cookiewise03 <258006403+cookiewise03@users.noreply.github.com>
2026-02-07 17:06:28 +0800

    Add files via upload

---
+++ disc/disc05/disc05.py
@@ -0,0 +1,139 @@
+#1.1
+def height(t):
+    """返回树的高度
+    >>> t = tree(3, [tree(5, [tree(1)]), tree(2)])
+    >>> height(t)
+    2
+    """
+    if is_leaf(t):
+        return 0
+    else:
+        return 1 + max([height(b) for b in branches(t)])
+#1.2
+def max_path_sum(t):
+    """返回树的最大路径和
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    if is_leaf(t):
+        return label(t)
+    else:
+        return label(t) + max([max_path_sum(b) for b in branches(t)])
+#1.3
+def square_tree(t):
+    """返回一棵新树，其每个节点值为原树对应节点值的平方
+    >>> numbers = tree(1,
+    ...                [tree(2,
+    ...                      [tree(3),
+    ...                       tree(4)]),
+    ...                 tree(5,
+    ...                      [tree(6,
+    ...                            [tree(7)]),
+    ...                       tree(8)])])
+    >>> print_tree(square_tree(numbers))
+    1
+      4
+        9
+        16
+      25
+        36
+          49
+        64
+    """    
+    return tree(label(t) ** 2, [square_tree(b) for b in branches(t)])
+#1.4
+def find_path(tree, x):
+    """
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10) # 返回 None
+    """
+    if label(tree) == x:
+        return [label(tree)]
+    for b in branches(tree):
+        path = find_path(b, x)
+        if path:
+            return [label(tree)] + path
+#2.2
+def prune_binary(t, nums):
+    if is_leaf(t):
+        if label(t) in nums:
+            return t
+        return None
+    else:
+        next_valid_nums = [n[1:] for n in nums if n[0] == label(t)]
+        new_branches = []
+        for b in branches(t):
+            pruned_branch = prune_binary(b, next_valid_nums)
+            if pruned_branch is not None:
+                new_branches = new_branches + [pruned_branch]
+        if not new_branches:
+            return None
+        return tree(label(t), new_branches)
+
+# Tree ADT
+
+
+def tree(label, branches=[]):
+    if change_abstraction.changed:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return {'label': label, 'branches': list(branches)}
+    else:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return [label] + list(branches)
+
+
+def label(tree):
+    if change_abstraction.changed:
+        return tree['label']
+    else:
+        return tree[0]
+
+
+def branches(tree):
+    if change_abstraction.changed:
+        return tree['branches']
+    else:
+        return tree[1:]
+
+
+def is_tree(tree):
+    if change_abstraction.changed:
+        if type(tree) != dict or len(tree) != 2:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+    else:
+        if type(tree) != list or len(tree) < 1:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+
+
+def is_leaf(tree):
+    return not branches(tree)
+
+
+def change_abstraction(change):
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
+
+
+def print_tree(t, indent=0):
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+
+def copy_tree(t):
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
